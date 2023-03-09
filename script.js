f0c10a1770faabc22582ea60b7546c7872d2b99c
Elnatan Samuel <elnatansamuel22@gmail.com>
2023-03-09 07:10:05 -0800

    new stuff

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ const grayScale = document.querySelector(".grayscale");
 const saturation = document.querySelector(".saturation");
 const chooseImg = document.querySelector(".chooseimg");
 const Createimg = document.querySelector(".createimg");
+const insertTxt = document.querySelector(".insertbtn");
+const saveBtn = document.querySelector(".savebtn");
+const addNewImg = document.querySelector(".addnewimg");
+const addimgBtn = document.querySelector(".newimagebtn");
+const pencilTool = document.querySelector(".pencildraw");
+const ExitTool = document.querySelector(".exitdraw");
+const removeTool = document.querySelector(".removeobj");
+const rectangle = document.querySelector(".rectangle");
+const triangle = document.querySelector(".triangle");
+const circle = document.querySelector(".circle");
+const pentagon = document.querySelector(".pentagon");
+const insertLine = document.querySelector(".insertline");
 
-var canvas = new fabric.Canvas("canvas");
+var canvas = new fabric.Canvas("canvas", {
+  preserveObjectStacking: true,
+});
 canvas.setWidth(document.body.scrollWidth);
 canvas.setHeight(600);
 
+//create image at first
+//
+//
+//
 document.querySelector(".createimg").onchange = function (e) {
   var reader = new FileReader();
   reader.onload = function (e) {
@@ -132,5 +150,224 @@ document.querySelector(".createimg").onchange = function (e) {
     crossOrigin: "anonymous";
   }
 };
+//
+//
+//
+//create image at first
 
-chooseImg.addEventListener("click", () => Createimg.click());
+//add image from add image button
+//
+//
+//
+document.querySelector(".addnewimg").onchange = function (e) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = function () {
+      var img = new fabric.Image(image);
+      img.set({
+        left: 400,
+        top: 100,
+      });
+      img.scaleToWidth(300);
+      canvas.add(img).setActiveObject(img).renderAll();
+
+      //
+      //
+      //filters
+      //
+      //
+      //
+
+      //filters
+      //
+      //
+    };
+  };
+  reader.readAsDataURL(e.target.files[0]);
+  {
+    crossOrigin: "anonymous";
+  }
+};
+
+//
+//
+//
+//add image from button
+
+const chooseImgFun = () => {
+  Createimg.click();
+  chooseImg.remove();
+};
+
+chooseImg.addEventListener("click", chooseImgFun);
+addimgBtn.addEventListener("click", () => addNewImg.click());
+
+const addText = () => {
+  var text = new fabric.Textbox("Type Here", {
+    width: 200,
+    fontSize: 20,
+    fill: "#000",
+    left: 100,
+    top: 100,
+  });
+
+  // Render the Textbox on Canvas
+  canvas.add(text);
+};
+
+insertTxt.addEventListener("click", addText);
+
+//save the canvas
+//
+//
+
+saveBtn.addEventListener("click", saveBtnFun);
+function saveBtnFun(e) {
+  this.href = canvas.toDataURL({
+    format: "png",
+  });
+  this.download = "canvas.png";
+}
+
+//
+///
+//save the canvas
+
+// Draw
+//
+//
+
+const drawWithPencil = () => {
+  canvas.isDrawingMode = true;
+  // var brush = new fabric.PencilBrush(canvas);
+  // var points = [
+  //   [10, 10],
+  //   [20, 20],
+  //   [25, 70],
+  //   [100, 300],
+  // ];
+
+  // brush.onMouseDown({ x: points[0][0], y: points[0][1] });
+  // for (var i = 1; i < points.length; i++) {
+  //   brush.onMouseMove({ x: points[i][0], y: points[i][1] });
+  // }
+};
+
+pencilTool.addEventListener("click", drawWithPencil);
+
+//
+//
+//Draw
+
+const erasePencil = () => {
+  canvas.isDrawingMode = true;
+  canvas.freeDrawingBrush = new fabric.EraserBrush(canvas);
+
+  //  optional
+  canvas.freeDrawingBrush.width = 10;
+
+  //  undo erasing
+  canvas.freeDrawingBrush.inverted = true;
+};
+
+ExitTool.addEventListener("click", () => (canvas.isDrawingMode = false));
+
+//
+//
+//remove
+
+const removeCurrent = () => {
+  canvas.getActiveObjects().forEach((obj) => {
+    canvas.remove(obj);
+  });
+  canvas.discardActiveObject().renderAll();
+};
+
+removeTool.addEventListener("click", removeCurrent);
+//
+//
+//remove
+
+//
+//
+//Shapes
+
+const rectTool = () => {
+  var rectangle = new fabric.Rect({
+    width: 100,
+    height: 100,
+    fill: "",
+    stroke: "green",
+    strokeWidth: 3,
+  });
+
+  // Render the Rect in canvas
+  canvas.add(rectangle);
+};
+
+const triangleTool = () => {
+  var triangle = new fabric.Triangle({
+    width: 300,
+    height: 150,
+    fill: "",
+    stroke: "green",
+    strokeWidth: 3,
+  });
+
+  // Render the Triangle in canvas
+  canvas.add(triangle);
+  canvas.centerObject(triangle);
+};
+
+const circleTool = () => {
+  var ellipse = new fabric.Ellipse({
+    rx: 40,
+    ry: 40,
+    fill: "",
+    stroke: "green",
+    strokeWidth: 3,
+  });
+
+  // Render the Ellipse in canvas
+  canvas.add(ellipse);
+};
+
+const pentTool = () => {
+  var polygon = new fabric.Polygon(
+    [
+      { x: 200, y: 10 },
+      { x: 250, y: 50 },
+      { x: 230, y: 100 },
+      { x: 170, y: 100 },
+      { x: 150, y: 50 },
+    ],
+    {
+      fill: "",
+      stroke: "green",
+      strokeWidth: 3,
+    }
+  );
+
+  // Render the polygon in canvas
+  canvas.add(polygon);
+};
+
+const insertLineFun = () => {
+  var line = new fabric.Line([50, 10, 200, 150], {
+    stroke: "green",
+  });
+
+  // Render the rectangle in canvas
+  canvas.add(line);
+};
+
+rectangle.addEventListener("click", rectTool);
+triangle.addEventListener("click", triangleTool);
+circle.addEventListener("click", circleTool);
+pentagon.addEventListener("click", pentTool);
+insertLine.addEventListener("click", insertLineFun);
+//
+//
+//Shapes
